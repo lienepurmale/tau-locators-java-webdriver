8fa4b9a7d402a6494057432eea24a763e8aca8dc
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-10-21 14:55:21 +0200

    Create SimpleWebTestWithPageObjects.java

---
+++ src/test/java/tests/SimpleWebTestWithPageObjects.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageobjects.DuckDuckGoResultsPage;
+import pageobjects.DuckDuckGoSearchPage;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleWebTestWithPageObjects {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void initWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\liene\\Desktop\\tau-locators-java-webdriver\\src\\main\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void searchDuckDuckGo() {
+        DuckDuckGoSearchPage searchPage = new DuckDuckGoSearchPage(driver);
+        searchPage.loadPage();
+        searchPage.search("giant panda");
+
+        DuckDuckGoResultsPage resultsPage = new DuckDuckGoResultsPage(driver);
+        List<String> linkTexts = resultsPage.getResultsLinkText("panda");
+
+        for (String text : linkTexts) {
+            assertTrue(text.matches("(?i).*panda.*"));
+        }
+    }
+
+    @AfterEach
+    public void quitWebDriver() {
+        driver.quit();
+    }
+}
